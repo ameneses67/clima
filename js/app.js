55dff5569d8f9624217d654af024f04ada706a9a
Poncho <ameneses67@gmail.com>
2023-03-21 19:08:20 -0600

    initial commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const container = document.querySelector(".container");
+const resultado = document.querySelector("#resultado");
+const formulario = document.querySelector("#formulario");
+
+window.addEventListener("load", () => {
+  formulario.addEventListener("click", buscarClima);
+});
+
+function buscarClima(e) {
+  e.preventDefault();
+
+  // validar llenado de campos
+
+  // consultar la api
+}
